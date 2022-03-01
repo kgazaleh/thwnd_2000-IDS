b5fb6e731a83b6f3db575d9b17450d54f18c6e36
kgazaleh <98373423+kgazaleh@users.noreply.github.com>
2022-02-28 22:55:59 -0500

    Add files via upload

---
+++ HW3/Life_Expectancy.sql
@@ -0,0 +1,201 @@
+select * from Life_Expectancy;
+
+SET SQL_SAFE_UPDATES = 0;
+delete from Life_Expectancy
+where Population = 0;
+
+select * from Life_Expectancy;
+
+select count(*) from Life_Expectancy;
+
+Select * from Life_Expectancy;
+
+SET SQL_SAFE_UPDATES = 0;
+delete from Life_Expectancy
+where Percentage_Expenditure = 0;
+Select * from Life_Expectancy;
+
+SET SQL_SAFE_UPDATES = 0;
+delete from Life_Expectancy
+where Life_Expectancy = 0;
+Select * from Life_Expectancy;
+
+select year, country, max(Adult_Mortality) from Life_Expectancy
+where Year = 2010;
+
+select year, country, min(Adult_Mortality) from Life_Expectancy
+where Year = 2010;
+
+select year, country, max(Adult_Mortality) from Life_Expectancy
+where Year = 2011;
+
+select year, country, min(Adult_Mortality) from Life_Expectancy
+where Year = 2011;
+
+select year, country, max(Adult_Mortality) from Life_Expectancy
+where Year = 2012;
+
+select year, country, min(Adult_Mortality) from Life_Expectancy
+where Year = 2012;
+
+select year, country, max(Adult_Mortality) from Life_Expectancy
+where Year = 2013;
+
+select year, country, min(Adult_Mortality) from Life_Expectancy
+where Year = 2013;
+
+select year, country, max(Adult_Mortality) from Life_Expectancy
+where Year = 2014;
+
+select year, country, min(Adult_Mortality) from Life_Expectancy
+where Year = 2014;
+
+select year, country, max(Adult_Mortality) from Life_Expectancy
+where Year = 2015;
+
+select year, country, min(Adult_Mortality) from Life_Expectancy
+where Year = 2015;
+
+select year, country, max(Population) from Life_Expectancy
+where Year = 2010;
+
+select year, country, min(Population) from Life_Expectancy
+where Year = 2010;
+
+select year, country, max(Population) from Life_Expectancy
+where Year = 2011;
+
+select year, country, min(Population) from Life_Expectancy
+where Year = 2011;
+
+select year, country, max(Population) from Life_Expectancy
+where Year = 2012;
+
+select year, country, min(Population) from Life_Expectancy
+where Year = 2012;
+
+select year, country, max(Population) from Life_Expectancy
+where Year = 2013;
+
+select year, country, min(Population) from Life_Expectancy
+where Year = 2013;
+
+select year, country, max(Population) from Life_Expectancy
+where Year = 2014;
+
+select year, country, min(Population) from Life_Expectancy
+where Year = 2014;
+
+select year, country, max(Population) from Life_Expectancy
+where Year = 2015;
+
+select year, country, min(Population) from Life_Expectancy
+where Year = 2015;
+
+select year, country, max(GDP) from Life_Expectancy
+where Year = 2010;
+
+select year, country, min(GDP) from Life_Expectancy
+where Year = 2010;
+
+select year, country, max(GDP) from Life_Expectancy
+where Year = 2011;
+
+select year, country, min(GDP) from Life_Expectancy
+where Year = 2011;
+
+select year, country, max(GDP) from Life_Expectancy
+where Year = 2012;
+
+select year, country, min(GDP) from Life_Expectancy
+where Year = 2012;
+
+select year, country, max(GDP) from Life_Expectancy
+where Year = 2013;
+
+select year, country, min(GDP) from Life_Expectancy
+where Year = 2013;
+
+select year, country, max(GDP) from Life_Expectancy
+where Year = 2014;
+
+select year, country, min(GDP) from Life_Expectancy
+where Year = 2014;
+
+select year, country, max(GDP) from Life_Expectancy
+where Year = 2015;
+
+select year, country, min(GDP) from Life_Expectancy
+where Year = 2015;
+
+select year, country, max(Schooling) from Life_Expectancy
+where Year = 2010;
+
+select year, country, min(Schooling) from Life_Expectancy
+where Year = 2010;
+
+select year, country, max(Schooling) from Life_Expectancy
+where Year = 2011;
+
+select year, country, min(Schooling) from Life_Expectancy
+where Year = 2011;
+
+select year, country, max(Schooling) from Life_Expectancy
+where Year = 2012;
+
+select year, country, min(Schooling) from Life_Expectancy
+where Year = 2012;
+
+select year, country, max(Schooling) from Life_Expectancy
+where Year = 2013;
+
+select year, country, min(Adult_Mortality) from Life_Expectancy
+where Year = 2013;
+
+select year, country, max(Schooling) from Life_Expectancy
+where Year = 2014;
+
+select year, country, min(Schooling) from Life_Expectancy
+where Year = 2014;
+
+select year, country, max(Schooling) from Life_Expectancy
+where Year = 2015;
+
+select year, country, min(Schooling) from Life_Expectancy
+where Year = 2015;
+
+select year, country, max(Alcohol) from Life_Expectancy
+where Year = 2010;
+
+select year, country, min(Alcohol) from Life_Expectancy
+where Year = 2010;
+
+select year, country, max(Alcohol) from Life_Expectancy
+where Year = 2011;
+
+select year, country, min(Alcohol) from Life_Expectancy
+where Year = 2011;
+
+select year, country, max(Alcohol) from Life_Expectancy
+where Year = 2012;
+
+select year, country, min(Alcohol) from Life_Expectancy
+where Year = 2012;
+
+select year, country, max(Alcohol) from Life_Expectancy
+where Year = 2013;
+
+select year, country, min(Alcohol) from Life_Expectancy
+where Year = 2013;
+
+select year, country, max(Alcohol) from Life_Expectancy
+where Year = 2014;
+
+select year, country, min(Alcohol) from Life_Expectancy
+where Year = 2014;
+
+select year, country, max(Alcohol) from Life_Expectancy
+where Year = 2015;
+
+select year, country, min(Alcohol) from Life_Expectancy
+where Year = 2015;
